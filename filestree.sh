41280cc2403390118425d7306cde4d8f1fb23b32
Arabella Brayer <Arabella.Brayer@ulb.ac.be>
2014-11-22 15:19:23 +0100

    apparemment, cette version fonctionne. Manque vérif des noms avec espace

--- filestree.sh
+++ filestree.sh
@@ -2,18 +2,25 @@
 #	INFO-F-201 											Projet "shell scripting"
 #	Author: Arabella BRAYER 000385657
 
+# function test()
+# {
+# 	echo -e "$1\c"
+# 	echo "$2"
+# }
+
 function display()
 {
 	for NODE in "$1"/*; do
-        echo "|--${NODE##*/}" 
+    echo -e "$2|--${NODE##*/}" 
 	if [[ -d $NODE ]]; then
-		echo -e "|  \c"
-		display "$NODE"
+		display "$NODE" "$2|  "
 	fi
 done
 }
 
 echo "$PWD"
-display "$PWD"
+display $PWD ""
+
+# test "|---" "blabla"
 
 exit 0
