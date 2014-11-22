3bd5043769d6ca5c1664b4af75fe5b3441c9d2d0
Arabella Brayer <Arabella.Brayer@ulb.ac.be>
2014-11-22 15:29:03 +0100

    les dossiers protected semblent tout de même fonctionner sans erreur. Att verif salle machine

--- filestree.sh
+++ filestree.sh
@@ -2,25 +2,17 @@
 #	INFO-F-201 											Projet "shell scripting"
 #	Author: Arabella BRAYER 000385657
 
-# function test()
-# {
-# 	echo -e "$1\c"
-# 	echo "$2"
-# }
-
-function display()
+function display() # fonction récursive qui prend deux paramètres : $1 et $2
 {
-	for NODE in "$1"/*; do
-    echo -e "$2|--${NODE##*/}" 
-	if [[ -d $NODE ]]; then
-		display "$NODE" "$2|  "
+	for NODE in "$1"/*; do   # pour chaque NODE du répertoire courant
+    echo -e "$2|--${NODE##*/}"  # écrit le nom préfixé de $2
+	if [[ -d $NODE ]]; then		# -d si c'est un dossier :
+		display "$NODE" "$2|  " # appel récursif de la fonction + "|   " 
 	fi
 done
 }
 
-echo "$PWD"
-display $PWD ""
-
-# test "|---" "blabla"
+echo "$PWD"		# écrit le répertoire courant avec le PATH complet
+display $PWD "" # envoie deux variables. La deuxième est vide à l'origine
 
 exit 0
