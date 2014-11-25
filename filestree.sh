a1a82f912fa45820c3eaa7b66ba636d44b56e2ae
Arabella Brayer <Arabella.Brayer@ulb.ac.be>
2014-11-25 15:14:01 +0100

    correction pour le traitement des noms de fichiers avec espaces

--- filestree.sh
+++ filestree.sh
@@ -5,10 +5,10 @@
 function display() # fonction récursive qui prend deux paramètres : $1 et $2
 {
 	for NODE in "$1"/*; do   # pour chaque NODE du répertoire courant
-    if [ ! -L $NODE ]; then
+    if [ ! -L "$NODE" ]; then
     echo -e "$2|--${NODE##*/}"  # écrit le nom préfixé de $2
 	fi
-	if [ -d $NODE ] && [[ ! -L $NODE ]]; then		# -d si c'est un dossier : -L pour ignorer les link
+	if [ -d "$NODE" ] && [[ ! -L "$NODE" ]]; then		# -d si c'est un dossier : -L pour ignorer les link
 		display "$NODE" "$2|  " # appel récursif de la fonction + "|   " 
 	fi
 done
