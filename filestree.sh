d28e82fdc2af7004fbc438577d157a5277553716
Arabella Brayer <Arabella.Brayer@ulb.ac.be>
2014-11-25 14:51:12 +0100

    Les liens symaboliques sont ignorés

--- filestree.sh
+++ filestree.sh
@@ -5,14 +5,16 @@
 function display() # fonction récursive qui prend deux paramètres : $1 et $2
 {
 	for NODE in "$1"/*; do   # pour chaque NODE du répertoire courant
+    if [ ! -L $NODE ]; then
     echo -e "$2|--${NODE##*/}"  # écrit le nom préfixé de $2
-	if [[ -d $NODE ]]; then		# -d si c'est un dossier :
+	fi
+	if [ -d $NODE ] && [[ ! -L $NODE ]]; then		# -d si c'est un dossier : -L pour ignorer les link
 		display "$NODE" "$2|  " # appel récursif de la fonction + "|   " 
 	fi
 done
 }
 
 echo "$PWD"		# écrit le répertoire courant avec le PATH complet
-display $PWD "" # envoie deux variables. La deuxième est vide à l'origine
+display $PWD "" # envoie deux variables. La deuxième est une chaîne vide à l'origine
 
 exit 0
