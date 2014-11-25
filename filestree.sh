dca3ea8f0c6e695d1796acdd9ff61d4c9c731ab8
Arabella Brayer <Arabella.Brayer@ulb.ac.be>
2014-11-25 18:39:49 +0100

    Ajustements de dernière minute. Envoi sur l'UV

--- filestree.sh
+++ filestree.sh
@@ -4,17 +4,17 @@
 
 function display() # fonction récursive qui prend deux paramètres : $1 et $2
 {
-	for NODE in "$1"/*; do   # pour chaque NODE du répertoire courant
-    if [ ! -L "$NODE" ]; then
+	for NODE in "$1"/*; do   	# pour chaque NODE du répertoire courant
+    if [ ! -L "$NODE" ]; then 	# -L pour ignorer les liens symboliques
     echo -e "$2|--${NODE##*/}"  # écrit le nom préfixé de $2
 	fi
-	if [ -d "$NODE" ] && [[ ! -L "$NODE" ]]; then		# -d si c'est un dossier : -L pour ignorer les link
+	if [ -d "$NODE" ] && [[ ! -L "$NODE" ]]; then		
 		display "$NODE" "$2|  " # appel récursif de la fonction + "|   " 
 	fi
 done
 }
 
-echo "$PWD"		# écrit le répertoire courant avec le PATH complet
-display $PWD "" # envoie deux variables. La deuxième est une chaîne vide à l'origine
+echo "$PWD/"		# écrit le répertoire courant avec le PATH complet
+display "$PWD" "" # envoie deux variables. La deuxième est une chaîne vide
 
 exit 0
